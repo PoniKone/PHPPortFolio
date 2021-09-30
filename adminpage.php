e752111e6cac7939e7d101928bd001efa147a2c6
PoniKone <91583857+PoniKone@users.noreply.github.com>
2021-09-30 15:38:09 +0900

    Add files via upload

---
+++ adminpage.php
@@ -0,0 +1,8 @@
+
+<hr>
+<h2 class="fs-22">管理者メニュー</h2>
+<ul class="account">
+	<li><a href="purchase-editadmin.php"><span class="fs-20">購入履歴データベース</span><br>キャンセル申請の受諾もこちら</a></li> <br>
+	<li><a href="customer-editadmin.php"><span class="fs-20">顧客情報管理</span><br>ユーザー情報の追加・変更はこちら</a></li> <br>
+	<li><a href="product-editadmin.php"><span class="fs-20">商品管理</span><br>商品の追加・変更はこちら</a></li> <br>
+</ul>
